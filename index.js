fa8cc4ee9f7eab8c2541214b8d163a69080fa32d
rustybucket-cloud <83788852+rustybucket-cloud@users.noreply.github.com>
2021-07-17 17:39:59 -0600

    publ

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 function fetchFact() {
-    fetch("https://cat-fact.herokuapp.com/facts/random")
+    fetch("https://brianiswu-cat-facts-v1.p.rapidapi.com/facts", {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-key": "c90b245b31msh46b59787848177ap15892cjsne103b05ba7a8",
+		"x-rapidapi-host": "brianiswu-cat-facts-v1.p.rapidapi.com"
+	}
+    })
         .then(res => res.json())
         .then(data => {
+            console.log(data);
             const randomFact = document.querySelector('#randomFact');
             randomFact.innerText = data.text;
         })
+        .catch(err => {
+            console.error(err);
+        });
 }
 document.querySelector('#button').addEventListener("click", fetchFact);
